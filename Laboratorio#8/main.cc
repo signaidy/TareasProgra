2c3846fee12b093a289538f26b1b8c6e974c42d3
signaidy <46787468+signaidy@users.noreply.github.com>
2019-03-21 09:18:32 -0600

    Add files via upload

---
+++ Laboratorio#8/main.cc
@@ -0,0 +1,13 @@
+#include "Figura.cc"
+#include "Rectangulo.cc"
+#inlude "Circulo.cc"
+
+void const abarcar(Figura& figura, Vector2d[] puntos, int cantidad){
+	
+}
+
+int main(){
+	char c;
+	cout<<"Ingrese la opcion que desea usar('v' para un punto, 'c' para un circulo, 'r' para un rectangulo): \n";
+	cin<<c;
+}
